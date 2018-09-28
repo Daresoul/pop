509779598c0c4c5434053e561dc06cc8ff9d96bd
Adrian Joensen <adrianvjoensen@gmail.com>
2018-09-28 10:32:55 +0200

    loop time change
    changed so it will now loop 300 instead of 3600

--- 4g/program.fsx
+++ 4g/program.fsx
@@ -1,6 +1,6 @@
 let circlecurcomfrance : float = 6.283185
 printfn "%15s %15s %15s %15s" "n" "polygon-value" "cirkel value" "diff"
-for i = 0 to 3600 do
+for i = 0 to 300 do
     let circumfrance : float = vec2d.polyLen i (float 1)
     let diff : float = circlecurcomfrance - circumfrance
     printfn "%15i %15f %15f %15f" i circumfrance circlecurcomfrance diff
