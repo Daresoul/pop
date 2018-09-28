40593afd6ddfffd82212cfeb8ef6c7f826962902
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-09-28 11:12:56 +0200

    changes to funktions dot produkt

--- 4g/program.fsx
+++ 4g/program.fsx
@@ -4,3 +4,4 @@ for i = 0 to 3600 do
     let circumfrance : float = vec2d.polyLen i (float 1)
     let diff : float = circlecurcomfrance - circumfrance
     printfn "%15i %15f %15f %15f" i circumfrance circlecurcomfrance diff
+
