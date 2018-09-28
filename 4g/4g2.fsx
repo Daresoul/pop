7998228cf855823cb044151a63aac938d8ece3e1
danishdude87 <32512765+danishdude87@users.noreply.github.com>
2018-09-28 10:50:55 +0200

    question b answered

--- 4g/4g2.fsx
+++ 4g/4g2.fsx
@@ -1,1 +1,3 @@
+//b)
 //For a polygone with n-> infinity the length (i.e. circumference) goes towords that of a perfect circle. 
+// This can be seen if one looks at a megagon (polygon with a million sides.) It is for the human eye a perfec circle. 
