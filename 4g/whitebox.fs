66c3a5eff54761fc759dd0a3709cf8c700d5fa10
danishdude87 <32512765+danishdude87@users.noreply.github.com>
2018-10-01 12:05:39 +0200

    Har sat formatet op i whitebox.

--- 4g/whitebox.fs
+++ 4g/whitebox.fs
@@ -18,24 +18,24 @@ let TuplesSignleInSingleOut (functionName : string) (tupeInput : float * float) 
 
 let TuplesSignleInFloatOut (functionName : string) (tupeInput : float * float) (output : float) (expected : float) =
     let (x, y) = tupeInput
-    sprintf "%s (%.2f, %.2f) %.2f %.2f" functionName x y output expected
-
+    sprintf "%s Vector (%.2f, %.2f) angel =>  %.2f == %.2f => %b" functionName x y output expected (output=expected)
+ 
 /// This is for 2 tupe input - single tupe output
 let TuplesTwoInSingleOut (functionName : string) (tupeInput1 : float * float) (tupeInput2 : float * float) (output : float * float) (expected : float * float) =
     let (x, y) = tupeInput1
     let (x1, y1) = tupeInput2
     let (x2, y2) = output
     let (ex, ey) = expected
-    sprintf "%s (%.2f, %.2f) (%.2f, %.2f) (%.2f, %.2f) (%.2f, %.2f)" functionName x y x1 y1 x2 y2 ex ey
+    sprintf "%s Vector (%.2f, %.2f) added vector (%.2f, %.2f) => (%.2f, %.2f) == (%.2f, %.2f) => %b" functionName x y x1 y1 x2 y2 ex ey (ex=ey)
 
 /// This is for 2 tupe input - single tupe output
 let TuplesTwoInFloatOut (functionName : string) (tupeInput1 : float * float) (tupeInput2 : float * float) (output : float) (expected : float) =
     let (x, y) = tupeInput1
     let (x1, y1) = tupeInput2
-    sprintf "%s %10s(%.2f, %.2f) %10s(%.2f, %.2f) %10s(%.2f) %10s(%.2f)" functionName "" x y "" x1 y1 "" output "" expected
+    sprintf "%s Vector (%.2f, %.2f) dot vector (%.2f, %.2f) => (%.2f) == (%.2f) => %b" functionName  x y  x1 y1  output expected (output = expected)
 
 let TupleAndConstInTupleOut (functionName : string) (tupeInput1 : float * float) (tupeInput2 : float) (output : float * float) (expected : float * float) =
     let (x, y) = tupeInput1
     let (x1, y1) = expected
     let (x2, y2) = output
-    sprintf "%s On given vector (%.2f, %.2f) multiply by (%.2f) we get (%.2f, %.2f) vs what we expect (%.2f, %.2f)" functionName x y tupeInput2 x2 y2 x1 y1
+    sprintf "%s Vector (%.2f, %.2f) multiply by (%.2f) => (%.2f, %.2f) == (%.2f, %.2f) => %b" functionName x y tupeInput2 x2 y2 x1 y1 (x2=x1 && y2 =y1)
