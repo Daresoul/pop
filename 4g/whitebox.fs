c168194709882557012b1ca9e91f0cb77fdb2500
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-10-01 11:43:11 +0200

    yass

--- 4g/whitebox.fs
+++ 4g/whitebox.fs
@@ -27,3 +27,15 @@ let TuplesTwoInSingleOut (functionName : string) (tupeInput1 : float * float) (t
     let (x2, y2) = output
     let (ex, ey) = expected
     sprintf "%s (%.2f, %.2f) (%.2f, %.2f) (%.2f, %.2f) (%.2f, %.2f)" functionName x y x1 y1 x2 y2 ex ey
+
+/// This is for 2 tupe input - single tupe output
+let TuplesTwoInFloatOut (functionName : string) (tupeInput1 : float * float) (tupeInput2 : float * float) (output : float) (expected : float) =
+    let (x, y) = tupeInput1
+    let (x1, y1) = tupeInput2
+    sprintf "%s %10s(%.2f, %.2f) %10s(%.2f, %.2f) %10s(%.2f) %10s(%.2f)" functionName "" x y "" x1 y1 "" output "" expected
+
+let TupleAndConstInTupleOut (functionName : string) (tupeInput1 : float * float) (tupeInput2 : float) (output : float * float) (expected : float * float) =
+    let (x, y) = tupeInput1
+    let (x1, y1) = expected
+    let (x2, y2) = output
+    sprintf "%s On given vector (%.2f, %.2f) multiply by (%.2f) we get (%.2f, %.2f) vs what we expect (%.2f, %.2f)" functionName x y tupeInput2 x2 y2 x1 y1
--- 4g/whitebox.fsx
+++ 4g/whitebox.fsx
@@ -7,9 +7,40 @@ vector <- vec2d.createVector 1.0 0.0
 printfn "%s" (whitebox.TuplesSignleInFloatOut functionName vector (vec2d.getAngle vector) 0.0)
 
 functionName <- "subtractVector"
-let mutable vector2 = vec2d.createVector 0.0 0.0
+let mutable vector2 = vec2d.createVector 1.0 2.0
 vector <- vec2d.createVector 0.0 0.0
-printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.subtractVector vector vector2) (0.0, 0.0))
+printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.subtractVector vector vector2) vector2)
+
+vector2 <- vec2d.createVector -1.0 0.0
+printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.subtractVector vector vector2) (-1.0, 2.0))
+
+functionName <- "addVectors"
+printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.addVectors vector vector2) vector)
+
+vector <- vec2d.createVector 1.0 2.0
+printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.addVectors vector vector2) (0.0, 2.0))
+
+vector2 <- vec2d.createVector 30.0 21.0
+printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.addVectors vector vector2) (31.0, 23.0))
+
+functionName <- "vectorProduct"
+vector <- vec2d.createVector 0.0 0.0
+printfn "%s" (whitebox.TuplesTwoInFloatOut functionName vector vector2 (vec2d.vectorProduct vector vector2) 0.0)
+
+vector <- vec2d.createVector (-30.0) (-21.0)
+printfn "%s" (whitebox.TuplesTwoInFloatOut functionName vector vector2 (vec2d.vectorProduct vector vector2) 0.0)
+
+
+vector <- vec2d.createVector 3.0 5.0
+vector2 <- vec2d.createVector 2.0 3.0
+printfn "%s" (whitebox.TuplesTwoInFloatOut functionName vector vector2 (vec2d.vectorProduct vector vector2) 21.0)
+
+functionName <- "multiplyVector"
+vector <- vec2d.createVector 0.0 0.0
+printfn "%s" (whitebox.TupleAndConstInTupleOut functionName vector (0.0) (vec2d.multiplyVector vector (0.0)) (0.0, 0.0))
+
+
+printfn "%s" (whitebox.TupleAndConstInTupleOut functionName vector2 (-1.0) (vec2d.multiplyVector vector2 (-1.0)) (-2.0, -3.0))
 
 //4g.2
 //b)
@@ -19,7 +50,3 @@ printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.s
 // Suggestion - Since the circle has 1 as radius, its circumference should be pi.
 // Meaning the bigger n is, the return value is closer to 2 * pi.
 
-
-vector <- vec2d.createVector (-1.0) 0.0
-vector2 <- vec2d.createVector 0.0 1.0
-printfn "%s" (whitebox.TuplesTwoInSingleOut functionName vector vector2 (vec2d.subtractVector vector vector2) (0.0, 0.0))
