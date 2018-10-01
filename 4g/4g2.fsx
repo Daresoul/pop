d52f7de5574940b3e400f46ff69c46a066bdd9c1
danishdude87 <32512765+danishdude87@users.noreply.github.com>
2018-10-01 10:58:35 +0200

    Har lavet opgave 4g3

--- 4g/4g2.fsx
+++ 4g/4g2.fsx
@@ -1,3 +1,4 @@
 //b)
-//For a polygone with n-> infinity the length (i.e. circumference) goes towords that of a perfect circle. 
-// This can be seen if one looks at a megagon (polygon with a million sides.) It is for the human eye a perfec circle. 
+// For a polygone with n-> infinity the length (i.e. circumference) goes towords that of a perfect circle. 
+// This can be seen if one looks at a megagon (polygon with a million sides.) It is for the human eye a perfec circle.
+// And you cant make a polygon with less than three sides/corners by their very definition (Smallest polygon has three sides in the plane.) The sides cant touch as they would collaps and become a line. If we were to did it omn a sphere you can do it. 
