44329d118532528b675742fc51b2fc6ba272d459
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-10-01 11:01:33 +0200

    cahnges to where the task is answered

--- 4g/4g2.fsx
+++ 4g/4g2.fsx
@@ -1,6 +1,7 @@
+// 4g.2
 //b)
 //For a polygone with n-> infinity the length (i.e. circumference) goes towords that of a perfect circle.
 // This can be seen if one looks at a megagon (polygon with a million sides.) It is for the human eye a perfec circle.
 
 // Suggestion - Since the circle has 1 as radius, its circumference should be pi.
-// Meaning the bigger n is, the return value is closer to pi.
+// Meaning the bigger n is, the return value is closer to 2 * pi.
--- 4g/whitebox.fsx
+++ 4g/whitebox.fsx
@@ -20,5 +20,11 @@ printfn "%s %25s(%f, %f) %30f %15f %15b" "getAngle" "" (fst vector) (snd vector)
 vector <- vec2d.createVector 0.0 (0.0)
 printfn "%s %25s(%f, %f) %30f %15f %15b %7b" "Subtract" "" (fst vector) (snd vector) (vec2d.getAngle vector) 0.00 (isbetween -0.001 0.001 ( fst vec2d.subtractVector (0.0 -1.0) (-1.0 0.0))) (isbetween -0.001 0.001 ( snd vec2d.subtractVector (0.0 -1.0) (-1.0 0.0)))
 
-vector <- vec2d.createVector 0.0 (-1.0)
-//printfn "%s %25s(%f, %f) %30f %15f %15b %7b" "Subtract" "" (fst vector) (snd vector) (vec2d.getAngle vector) 0.00 (isbetween -0.001 0.001 ( fst vec2d.subtractVector vector vector)) (isbetween -0.001 0.001 ( snd vec2d.subtractVector vector vector))
+
+//4g.2
+//b)
+//For a polygone with n-> infinity the length (i.e. circumference) goes towords that of a perfect circle.
+// This can be seen if one looks at a megagon (polygon with a million sides.) It is for the human eye a perfec circle.
+
+// Suggestion - Since the circle has 1 as radius, its circumference should be pi.
+// Meaning the bigger n is, the return value is closer to 2 * pi.
