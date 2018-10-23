6365f4663c38432c943e4ff81a129313f3cf8180
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-10-23 16:15:32 +0200

    wertyu

--- 6g/6g3.fsx
+++ 6g/6g3.fsx
@@ -1,3 +1,5 @@
+open System.Numerics
+open System
 let cfrac2float (lst: int list) (i : int) : float =
     let newint = i+1
     let rec hahaha (lst : int list) (r : int) =
@@ -21,7 +23,7 @@ let cfrac2float (lst: int list) (i : int) : float =
 let rec decimal2fraction(number: float) : float =
     let r = ceil number
     let r2 = number - r
-    let r3 = -0.1
+    let r3 = -0.00001
     if (r2<r3) then 
         decimal2fraction(number*10.0)
     else 
@@ -29,19 +31,29 @@ let rec decimal2fraction(number: float) : float =
 
 let cfrac2frac(lst: int list)(i: int): int*int=
     let floatValue = cfrac2float (lst) (i)
-    let tæller = decimal2fraction floatValue
+    let tæller = ceil (decimal2fraction floatValue)
     let nævner = tæller / floatValue
+    let tæller2 = bigint tæller
+    let nævner2 = bigint nævner
     printfn "%f %f %f" floatValue tæller nævner
-    ((int tæller), (int nævner))
+    let gcd = (BigInteger.GreatestCommonDivisor(tæller2,nævner2))
+    let returnTæller = (tæller2/gcd)
+    let returnNævner = (nævner2/gcd)
+    printfn "%A %A %A"gcd returnTæller returnNævner
+    (int returnNævner,int returnTæller)
 
+
+let cfToScalar cf = List.foldBack (fun elem acc -> float elem + (1.0 / float acc)) cf System.Double.MaxValue
 
 [<EntryPoint>]
 let main args =
-    let lst = [3;7;15;1;292]
+    let lst = [3;7;15;1;292;1]
     let lst2 = [3;4;12;4]
     let lst3 = []
 
-    printfn "The list %A makes the continued fraction: %A" lst (cfrac2frac (lst) (3))
-    //printfn "The list %A makes the continued fraction: %f" lst2 (cfrac2float (lst2) (2))
-    //printfn "The list %A makes the continued fraction: %f" lst3 (cfrac2float (lst3) (1))
+    printfn "%f" (cfToScalar lst)
+
+    //printfn "The list [3;7;15;1;292;1] makes the continued fraction: %i %i" <|| (cfrac2frac (lst) (3))
+    //printfn "The list [3;4;12;4] makes the continued fraction: %i %i" <|| (cfrac2frac (lst2) (3)) 
+    //printfn "The list [] makes the continued fraction: %i %i" <|| (cfrac2frac (lst3) (1))
     0
