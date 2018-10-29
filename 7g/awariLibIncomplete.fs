7ae1f4c9fbc997ad34770e91b01ba027a4915c72
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-10-29 11:57:12 +0100

    changed so that it will now load functions

--- 7g/awariLibIncomplete.fs
+++ 7g/awariLibIncomplete.fs
@@ -5,35 +5,6 @@ type player = Player1 | Player2
 
 // intentionally many missing implementations and additions
 
-let turn (b : board) (p : player) : board =
-  let rec repeat (b: board) (p: player) (n: int) : board =
-    printBoard b
-    let str =
-      if n = 0 then
-        sprintf "Player %A's move? " p
-      else 
-        "Again? "
-    let i = getMove b p str
-    let (newB, finalPitsPlayer, finalPit)= distribute b p i
-    if not (isHome b finalPitsPlayer finalPit) 
-       || (isGameOver b) then
-      newB
-    else
-      repeat newB p (n + 1)
-  repeat b p 0 
-
-let rec play (b : board) (p : player) : board =
-  if isGameOver b then
-    b
-  else
-    let newB = turn b p
-    let nextP =
-      if p = Player1 then
-        Player2
-      else
-        Player1
-    play newB nextP
-
 let isGameOver (b : board) : bool =
   if b.IsEmpty then
     true
@@ -69,3 +40,32 @@ let isHome (b : board) (p : player) (i : pit) : bool =
         true
       else
         false
+
+let turn (b : board) (p : player) : board =
+  let rec repeat (b: board) (p: player) (n: int) : board =
+    printBoard b
+    let str =
+      if n = 0 then
+        sprintf "Player %A's move? " p
+      else 
+        "Again? "
+    let i = getMove b p str
+    let (newB, finalPitsPlayer, finalPit)= distribute b p i
+    if not (isHome b finalPitsPlayer finalPit) 
+       || (isGameOver b) then
+      newB
+    else
+      repeat newB p (n + 1)
+  repeat b p 0 
+
+let rec play (b : board) (p : player) : board =
+  if isGameOver b then
+    b
+  else
+    let newB = turn b p
+    let nextP =
+      if p = Player1 then
+        Player2
+      else
+        Player1
+    play newB nextP
