f8b8bac31a75c98814faf2762d7039572ec223b4
danishdude87 <32512765+danishdude87@users.noreply.github.com>
2018-11-23 11:37:35 +0100

    Update testPNG.fsx

--- 8g/testPNG.fsx
+++ 8g/testPNG.fsx
@@ -1,3 +1,5 @@
+
+
 let bmp = ImgUtil .mk 256 256  //<-- lav et bmp som er 256 x 256 pixel stort
 
 
@@ -10,6 +12,9 @@ do ImgUtil . setLine (ImgUtil . fromRgb (255,255,255)) (0,255) (255,0) bmp
 
 //Laver en vinkelret firkant mellem (50;50) og (205;205)
 do ImgUtil . setBox (ImgUtil . fromRgb (255,255,255)) (50,50) (205,205) bmp
+
+//circle coordinate equation (x – h)^2 + (y – k)^2 = r2 where h and k are the center. Man burde kunen lave en funktion som ser om afstanden dist1 til (h;k) =< afstanden dist2 som er afstanden fra centrum. (Vi kunne i princis lave bounding box her, men lad os gemme det til senere)
+
 
 //Konventer bmp til en png fil. 
 do ImgUtil . toPngFile " test .png" bmp
