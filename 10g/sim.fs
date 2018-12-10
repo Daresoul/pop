7e12d06c7e95f12db5e9d2806710f68dcc0c40a8
Nicolas Bærbar <Nicolas.dyhrman@gmail.com>
2018-12-10 11:22:59 +0100

    created most logic for animals

--- 10g/sim.fs
+++ 10g/sim.fs
@@ -1,3 +1,6 @@
+type position = int * int
+type tick = int
+
 type Game() = class
     // Holds all game settings (like how many ticks and how many ticks per birth and so on)
 end
@@ -7,16 +10,43 @@ type Board() = class
     // like all the animals in 2 lists
 end
 
-type Animal() = class
+type Animal (pos : position, birthTicks : tick, maxAge : tick) = class
     // the data of an animal all has
+    let mutable _remaindingBirthTicks = birthTicks
+    let mutable _pos = (fst pos, snd pos)
+    let mutable _age = 0
+    member this.Pos = _pos
+    member this.BirthTicks = birthTicks
+    member this.RemaindingBirthTicks = _remaindingBirthTicks
+    
+    member this.Move(x,y) =
+        _pos <- ((fst _pos) + x, (snd _pos) + y)
+    
+    member this.GiveBirth = 0
+    member this.UpdateBirthTicks = _remaindingBirthTicks <- _remaindingBirthTicks - 1
+    member this.ResetReproduction = _remaindingBirthTicks <- this.BirthTicks
+
+    member this.MaxAge = maxAge
+    member this.IncreaseAge = _age <- _age + 1
+    member this.CheckAge =
+        if _age = this.MaxAge then
+            //kill this
+            1
+        else
+            // Dont kill it
+            0
 end
 
-type Moose() = class
-    inherit Animal()
-    // the data only mooses has
+type Moose(pos : position, birthTicks : tick, maxAge : tick) = class
+    inherit Animal(pos, birthTicks, maxAge)
 end
 
-type Wolf() = class
-    inherit Animal()
-    // The data only wolfs has
+type Wolf(pos : position, birthTicks : tick, maxAge : tick, hungerTick : tick) = class
+    inherit Animal(pos, birthTicks, maxAge)
+    let mutable _hunger = hungerTick
+    member this.Hunger = _hunger
+    member this.HungerTick = hungerTick 
+
+    member this.DecreaseHunger = _hunger - 1
+    member this.ResetHunger = _hunger <- this.HungerTick
 end
